aebed7d74980b418bde2833ce1c8ff8b2fb2a23c
Lucy <makepin2r@gmail.com>
2021-06-11 10:00:25 +0900

    mousemove 함수 실습

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -21,6 +23,10 @@ const app = Vue.createApp({
             if(data) {
                 console.log(data);
             }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 })
